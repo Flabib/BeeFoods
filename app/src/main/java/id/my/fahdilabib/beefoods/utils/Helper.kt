67105004721c9d455e00ad37393356001d457e3c
Flabib <fahdilabib@gmail.com>
2021-03-08 16:14:40 +0700

    remove unused component based on inspect code

--- app/src/main/java/id/my/fahdilabib/beefoods/utils/Helper.kt
+++ app/src/main/java/id/my/fahdilabib/beefoods/utils/Helper.kt
@@ -1,12 +1,7 @@
 package id.my.fahdilabib.beefoods.utils
 
-import android.app.Activity
 import android.content.Context
-import android.graphics.drawable.Drawable
 import android.widget.ImageView
-import androidx.core.content.ContextCompat
-import androidx.core.content.res.ResourcesCompat
-import androidx.core.graphics.drawable.DrawableCompat
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import java.text.NumberFormat
@@ -14,13 +9,6 @@ import java.util.*
 
 class Helper {
     companion object {
-        fun changeDrawableColor(activity : Activity, id : Int, color : Int) {
-            val baseIconProfile : Drawable? = ResourcesCompat.getDrawable(activity.resources, id, null)
-            val iconProfile : Drawable? = baseIconProfile?.let { DrawableCompat.wrap(it) }
-            iconProfile?.let { DrawableCompat.setTint(it, ContextCompat.getColor(activity.baseContext, color)) }
-        }
-
-
         fun applyImage(img: Int, context: Context, imageView: ImageView) {
             Glide.with(context)
                     .apply { RequestOptions().centerCrop() }
