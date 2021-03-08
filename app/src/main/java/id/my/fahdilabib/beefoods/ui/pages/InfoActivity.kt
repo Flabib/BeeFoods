8c5fe73a4bafa2a5cb2b06e3cc08970e11664d07
Flabib <fahdilabib@gmail.com>
2021-03-08 13:05:38 +0700

    change info view design and themes.xml

--- app/src/main/java/id/my/fahdilabib/beefoods/ui/pages/InfoActivity.kt
+++ app/src/main/java/id/my/fahdilabib/beefoods/ui/pages/InfoActivity.kt
@@ -1,5 +1,7 @@
 package id.my.fahdilabib.beefoods.ui.pages
 
+import android.graphics.text.LineBreaker.JUSTIFICATION_MODE_INTER_WORD
+import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import id.my.fahdilabib.beefoods.databinding.ActivityInfoBinding
@@ -8,6 +10,12 @@ class InfoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityInfoBinding.inflate(layoutInflater)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            binding.myDesc.justificationMode = JUSTIFICATION_MODE_INTER_WORD
+            binding.myName.justificationMode = JUSTIFICATION_MODE_INTER_WORD
+            binding.myJob.justificationMode = JUSTIFICATION_MODE_INTER_WORD
+        }
 
         setContentView(binding.root)
     }
